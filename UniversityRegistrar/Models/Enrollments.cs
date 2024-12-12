9b249d0bce9859466f43f8605d7a0f15ff407e5e
igl-myrick <igl.myrick@gmail.com>
2024-12-12 15:54:59 -0800

    add Enrollments class

---
+++ UniversityRegistrar/Models/Enrollments.cs
@@ -0,0 +1,9 @@
+namespace UniversityRegistrar.Models
+{
+  public class Enrollments
+  {
+    public int Id { get; set; }
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+  }
+}
