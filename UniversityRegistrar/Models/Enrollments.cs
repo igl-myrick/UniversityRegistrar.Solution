0238540c11656f772cbd5ecb91167d60373d08f1
igl-myrick <igl.myrick@gmail.com>
2024-12-16 15:49:57 -0800

    rename to Enrollment and add navigation properties

--- UniversityRegistrar/Models/Enrollments.cs
+++
@@ -1,9 +0,0 @@
-namespace UniversityRegistrar.Models
-{
-  public class Enrollments
-  {
-    public int Id { get; set; }
-    public int StudentId { get; set; }
-    public int CourseId { get; set; }
-  }
-}
