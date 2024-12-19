fc7f37db5a5fc6a7116602f14620b0cc78dcc5dd
igl-myrick <igl.myrick@gmail.com>
2024-12-19 14:26:04 -0800

    add Index route

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using UniversityRegistrar.Models;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Student> model = _db.Students
+        .ToList();
+      return View(model);
+    }
+  }
+}
